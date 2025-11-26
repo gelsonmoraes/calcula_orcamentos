210af2d937d8c997659a4b64b93bc19f7522825e
gelsonmoraesds-source <gelsonmoraesds@gmail.com>
2025-11-26 00:08:43 -0300

    Corrige o nome das páginas

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ st.title("Calculadora de Orçamento")
 
 #Definição das páginas
 
-mao_de_obra = st.Page("pages/2_Mao_de_Obra.py", title="Mão de Obra", icon="👷🏾")
-materiais = st.Page("pages/3_Materiais.py", title="Materiais", icon="🧱")
-tecidos = st.Page("pages/4_Tecidos.py", title="Tecidos", icon="🧵")
-pecas = st.Page("pages/5_Pecas.py", title="Peças", icon="🧩")
+mao_de_obra = st.Page("pages/1_Mao_de_Obra.py", title="Mão de Obra", icon="👷🏾")
+materiais = st.Page("pages/2_Materiais.py", title="Materiais", icon="🧱")
+tecidos = st.Page("pages/3_Tecidos.py", title="Tecidos", icon="🧵")
+pecas = st.Page("pages/4_Pecas.py", title="Peças", icon="🧩")
 
 pg = st.navigation(pages=[mao_de_obra, materiais, tecidos, pecas])
 st.sidebar.caption("Calculadora de Orçamento")
